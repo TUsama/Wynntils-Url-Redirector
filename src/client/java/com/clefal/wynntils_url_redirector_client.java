92966b7968585832aa72cc1f7a266a7fe347f188
TUsama <1716976420@qq.com>
2024-09-20 01:25:05 +0800

    why

--- src/client/java/com/clefal/wynntils_url_redirector_client.java
+++ src/client/java/com/clefal/wynntils_url_redirector_client.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 public class wynntils_url_redirector_client implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> ChangeModeCommand.register(dispatcher)));
 	}
 }
